c7ec2c43a0f1df3be462a2a7bf330cb3904f5bdf
Arjun Chakkalakunnel <arjuncmohanan@gmail.com>
2016-04-04 15:52:07 +0530

    udate app.js
    change server port 8089 nto 8081

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var routes = require('./routes');
 
 var app = express();
 app.set('domain', 'arjun.com');
-app.set('port', process.env.PORT || 8089);
+app.set('port', process.env.PORT || 8081);
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
 connection.init();
 routes.configure(app);
 
-var server = app.listen(8089, function() {
+var server = app.listen(8081, function() {
 	
   console.log('Server listening on port ' + server.address().port);
 });
